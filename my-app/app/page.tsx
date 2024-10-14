be57fa21c5f31dcf8264305127371ccee9988a52
Zola <Zelalemyohannes75@gmail.com>
2024-10-14 11:34:37 +0300

    commited

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600">Hello World</h1>
+    </div>
+  );
+}
